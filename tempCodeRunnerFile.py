df5c6bd83d5b8e809a7356512c5d7aaac9c589e5
GonzaloSerpino <serpinogonzalo@gmail.com>
2026-05-14 17:22:22 -0300

    Estadisticas y optimizacion de funciones

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -76,13 +76,41 @@ def validar_inputs(valor, flag=0):
     #Si todos los inputs cumplen con las condiciones devuelvo True, sino devuelvo False.
     return bool(flag_valor)
 
-def buscar_pais(nombre_pais):
+def buscar_pais(valor,tipo_busqueda):
     """Busco el nombre del pais, si esta dentro de la lista, retorno True, sino False"""
     global paises
-    for pais in paises:
-        if pais["nombre"] == nombre_pais:
-            return True, pais
-    return False, None
+    #seteo por default el valor de la bandera
+    flag = 0
+    #Genero una lista temporal ya que las busquedas por poblacion, superficie y continente pueden tener mas de un resultado
+    lista_paises_temp = []
+    #Si el tipo de busqueda es 1, busco el pais por nombre
+    if tipo_busqueda == 1:
+        for pais in paises:
+            if pais["nombre"] == valor:
+                return True, pais
+    #Si es 2, lo busco por poblacion
+    elif tipo_busqueda == 2:
+        for pais in paises:
+            if pais["poblacion"] == valor:
+                lista_paises_temp.append(pais)
+                flag = 1
+    #Si es 3, lo busco por superficie
+    elif tipo_busqueda == 3:
+        for pais in paises:
+            if pais["superficie"] == valor:
+                lista_paises_temp.append(pais)
+                flag = 1
+    #Si es 4, lo busco por continente
+    elif tipo_busqueda == 4:
+        for pais in paises:
+            if pais["continente"] == valor:
+                lista_paises_temp.append(pais)
+                flag = 1
+    
+    if flag: 
+        return True, lista_paises_temp
+    else:
+        return False, None
 
 def modificacion_pais(pais):
     """Menu y modificacion de pais"""
@@ -144,7 +172,7 @@ def actualizar_pais():
             print("Valor de nombre invalido, por favor ingrese un valor correcto.")
             nombre = input("Ingrese el nombre del pais a actualizar: ").title()
         #me traigo la flag si el pais esta dentro de la lista o no
-        flag, pais = buscar_pais(nombre)
+        flag, pais = buscar_pais(nombre,1)
         #si esta dentro de la lista, reemplazo el pais existente con los datos modificados.
         if flag:
             pais_modificado = modificacion_pais(pais)
@@ -159,9 +187,115 @@ def actualizar_pais():
     except Exception as e:
         print(f"Se encontro el siguiente error: {e}")
 
-paises = obtener_paises(NOMBRE_ARCHIVO)
-actualizar_pais()
+def busqueda():
+    """Menu y busqueda de paises"""
+    try:
+        global paises
+        match_paises = []
+        print("------------ Busqueda Pais ------------")
+        print(f"Como desea realizar la busqueda?")
+        print("1. Por Nombre\n2. Por Poblacion\n3. Por Superficie\n4. Por Continente")
+        opcion = int(input("Seleccione una opcion: "))
+    
+        #Si la opcion es 1, busco por nombre
+        if opcion == 1:
+            valor_busqueda = input("Ingrese el nombre del pais a buscar: ").title()
+            while not validar_inputs(valor_busqueda):
+                print("Valor de nombre invalido, por favor ingrese un valor correcto.")
+                valor_busqueda = input("Ingrese el nombre del pais a buscar: ").title()
+            
+            #Traigo el flag que me va a indicar si encontro el pais y el contenido de ese pais
+            flag, match_paises = buscar_pais(valor_busqueda,opcion)
+            if flag: 
+                print(f"Pais: {match_paises["nombre"]} | Poblacion: {match_paises["poblacion"]} | Superficie: {match_paises["superficie"]} | Continente: {match_paises["continente"]}")
+            else:
+                print("No existe un pais con ese nombre, agregelo a la lista.")
+                
+        #Me guardo el input de la poblacion a buscar
+        elif opcion == 2:
+            valor_busqueda = input("Ingrese el valor de la poblacion a buscar: ")
+            while not validar_inputs(valor_busqueda,1):
+                print("Valor de poblacion invalido, por favor ingrese un valor correcto.")
+                valor_busqueda = input("Ingrese el valor de la poblacion a buscar: ")
+            valor_busqueda = int(valor_busqueda)
+            
+        #Me guardo el input de la superficie a buscar
+        elif opcion == 3:
+            valor_busqueda = input("Ingrese el valor de la superficie a buscar: ")
+            while not validar_inputs(valor_busqueda,1):
+                print("Valor de superficie invalido, por favor ingrese un valor correcto.")
+                valor_busqueda = input("Ingrese el valor de la superficie a buscar: ")
+            valor_busqueda = int(valor_busqueda)
+            
 
+        #Me guardo el input del continente a buscar
+        elif opcion == 4:
+            valor_busqueda = input("Ingrese el valor del continente a buscar: ")
+            while not validar_inputs(valor_busqueda):
+                print("Valor de continente invalido, por favor ingrese un valor correcto.")
+                valor_busqueda = input("Ingrese el valor del continente a buscar: ")
+              
+        else:
+            print("Valor de opcion incorrecta, intente nuevamente")
+        
+        #Como pueden ser mas de un pais que puede compartir el mismo valor en poblacion, contiente y superficie, los englobo en un solo if
+        if opcion >= 2 and opcion <= 4:
+            flag, match_paises = buscar_pais(valor_busqueda,opcion)
+            if flag:
+                for pais in match_paises:
+                    print(f"Pais: {pais["nombre"]} | Poblacion: {pais["poblacion"]} | Superficie: {pais["superficie"]} | Continente: {pais["continente"]}")
+            else:
+                print("No existen paises con ese numero de poblacion/superficie/continente indicado.")
+    except ValueError:
+        print("Valor de opcion incorrecto, por favor intenta nuevamente.")
+    except Exception as e:
+        print(f"Se produjo el siguiente error: {e}")
+    
+def promedio_superficie():
+    global paises
+    acu = 0
+    count = 0
+    for pais in paises:
+        acu += pais["superficie"]
+        count += 1
+    
+    promedio = float(acu / count)
+    
+    return promedio
+    
+def paises_continentes():
+    
+    global paises
+    # Diccionario con relacion Continente - Cantidad de paises
+    conteo = {} 
+
+    #Recorro la lista global de paises
+    for pais in paises:
+        nombre_continente = pais["continente"]
+        
+        if nombre_continente in conteo:
+            conteo[nombre_continente] += 1
+        else:
+            conteo[nombre_continente] = 1
+            
+    # # Si necesitas retornar el set y la lista por separado como tenías pensado:
+    
+    continentes_unicos = list(conteo.keys())
+    cantidades = list(conteo.values())
+    
+    return continentes_unicos, cantidades
+    
+    
+paises = obtener_paises(NOMBRE_ARCHIVO)
+#actualizar_pais()
+#busqueda()
+
+promedio = promedio_superficie()
+print(f"El promedio de la superficie entre los paises es de: {promedio}")
+
+continentes, cantidad = paises_continentes()
+for i in range(len(continentes)):
+    print(f"Continente: {continentes[i]} | Cantidad de paises: {cantidad[i]}")
 #paises[0]["nombre"] = "Brasil"
 #print(paises[0]["nombre"])
 #actualizar_archivo(NOMBRE_ARCHIVO, paises, CAMPOS)
